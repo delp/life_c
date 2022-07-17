e273d3cf9f3fc72983704d621f00fa2dd51b2e82
Alex R. Delp <ardelp@gmail.com>
2022-07-17 12:09:54 -0700

    Basic neighbor mapping logic. Need to fix stepping outside of valid memory range

--- life.c
+++ life.c
@@ -3,12 +3,12 @@
 
 void draw_world(int* world, int w, int h) {
 
-    for(int i = 0; i < w; i++) {
-        for(int j=0; j < h; j++) {
-            int val = world[i * w + j];
+    for(int y = 0; y < w; y++) {
+        for(int x=0; x < h; x++) {
+            int val = world[y * w + x];
             if (val == 1) { 
                 printf("* ");
-            } else {
+            } else if (val == 0) {
                 printf("_ ");
             }
         }
@@ -17,9 +17,9 @@ void draw_world(int* world, int w, int h) {
 }
 
 void zero_map(int* world, int w, int h) {
-    for(int i = 0; i < w; i++) {
-        for(int j = 0; j < h; j++) {
-            world[i * w + j] = 0;
+    for(int y = 0; y < w; y++) {
+        for(int x = 0; x < h; x++) {
+            world[y * w + x] = 0;
         }
     }
 }
@@ -30,6 +30,55 @@ int* get_map(int w, int h) {
 
 void update_world(int* map, int w, int h) {
 
+    int nb_map[25];
+    //for each
+    //count neighbors
+    //update neighbor count
+    //kill all lowbies
+    //birth all babbys
+
+    //need a neighbor location algorithm
+
+    for(int y = 0; y < w; y++) {
+        for(int x = 0; x < h; x++) {
+
+		int ul = map[(y-1)*w + (x-1)];
+		int uc = map[(y-1)*w + x];
+		int ur = map[(y-1)*w + (x+1)];
+
+		int l = map[(y)*w + (x-1)];
+		int r = map[(y)*w + (x+1)];
+
+		int ll = map[(y+1)*w + (x-1)];
+		int lc = map[(y+1)*w + x];
+		int lr = map[(y+1)*w + (x+1)];
+
+		int num_neighbors = ul + uc + ur + l + r + ll + lc + lr;
+		printf("%d ", num_neighbors);
+		//printf("%d %d %d\n", ul, uc, ur);
+		//printf("%d %d %d\n", l, map[y*w + x], r);
+		//printf("%d %d %d\n", ll, lc, lr);
+
+        }
+	printf("\n");
+    }
+}
+
+void simple_start(int* map, int w, int h) {
+
+   /* 0,0,0,0,0
+      0,0,1,1,0,
+      0,0,1,0,0,
+      0,0,1,0,0,
+      0,0,1,0,0,
+   */
+    
+    int desired[] = {0,0,0,0,0,0,0,1,1,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0};
+    for(int y = 0; y < w; y++) {
+        for(int x = 0; x < h; x++) {
+            map[y * w + x] = desired[y * w + x];
+        }
+    }
 }
 
 int main() {
@@ -38,6 +87,7 @@ int main() {
     int* map = get_map(w, h);
     
     zero_map(map, w, h);
+    simple_start(map, w, h);
 
     while(1) {
         draw_world(map, w, h);
